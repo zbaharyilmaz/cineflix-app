05efd25eb09ef2e162baa7a6d53ab12b48de974c
zeynepyilmaz <ylmzbhr1@gmail.com>
2025-03-15 17:54:48 +0300

    Refactor Navbar component: conditionally render menu items based on user authentication and current page location

--- src/components/organisms/Navbar.jsx
+++ src/components/organisms/Navbar.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   Disclosure,
   Menu,
@@ -5,7 +6,7 @@ import {
   MenuItem,
   MenuItems,
 } from "@headlessui/react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom"; // Import useLocation
 import SwitchButton from "../atoms/SwitchButton";
 import { Avatar } from "@mui/material";
 import { AuthPage } from "../../context/AuthContext";
@@ -17,6 +18,10 @@ function classNames(...classes) {
 
 export default function Navbar() {
   const { logout, currentUser } = useContext(AuthPage);
+  const location = useLocation(); // Get the current location
+
+  // Only show the full navbar if the user is not on the login or register page
+  const isLoginOrRegisterPage = location.pathname === "/login" || location.pathname === "/register";
 
   return (
     <Disclosure
@@ -35,72 +40,75 @@ export default function Navbar() {
             </Link>
             <SwitchButton className="sm:hidden" />
           </div>
-          <div className="sm:hidden mt-10 mr-4">
-            <Disclosure.Button className="text-color2-lightblue dark:text-color1-pink focus:outline-none">
-              <span className="sr-only">Open menu</span>
-              <svg
-                className="h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            </Disclosure.Button>
-          </div>
-          <div className="hidden sm:flex items-center gap-2">
-            {currentUser ? (
-              <div className="absolute inset-y-0 left-0 items-center pr-2 sm:static sm:inset-auto flex gap-2 ">
-                <h5 className="text-color8-grey dark:text-color6-lightgrey mt-8 m-1 md:text-[1.1rem]">
-                  {currentUser?.displayName}
-                </h5>
-                <Menu as="div" className="relative ml-3">
+
+          {!isLoginOrRegisterPage && (
+            <div className="sm:hidden mt-10 mr-4">
+              <Disclosure.Button className="text-color2-lightblue dark:text-color1-pink focus:outline-none">
+                <span className="sr-only">Open menu</span>
+                <svg
+                  className="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              </Disclosure.Button>
+            </div>
+          )}
+          {!isLoginOrRegisterPage && (
+            <div className="hidden sm:flex items-center gap-2">
+              {currentUser ? (
+                <div className="absolute inset-y-0 left-0 items-center pr-2 sm:static sm:inset-auto flex gap-2 ">
+                  <h5 className="text-color8-grey dark:text-color6-lightgrey mt-8 m-1 md:text-[1.1rem]">
+                    {currentUser?.displayName}
+                  </h5>
                   <div className="mt-7">
                     <MenuButton className="relative flex rounded-full  text-sm focus:outline-none">
                       <span className="absolute -inset-1.5" />
                       <span className="sr-only">Open user menu</span>
                       <Avatar src={currentUser?.photoURL || Avatar} />
                     </MenuButton>
+                    <MenuItems
+                      transition
+                      className="absolute right-0 top-20 z-50 mt-2 origin-top-right rounded-md bg-color7-light py-1 shadow-lg ring-1 ring-color6-lightgrey transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in min-w-20"
+                    >
+                      <MenuItem>
+                        <span
+                          className="block px-4 py-2 text-sm text-color5-dark data-[focus]:bg-color6-lightgrey data-[focus]:outline-none"
+                          onClick={() => logout()}
+                        >
+                          Logout
+                        </span>
+                      </MenuItem>
+                    </MenuItems>
                   </div>
-                  <MenuItems
-                    transition
-                    className="absolute right-0 top-20 z-50 mt-2  origin-top-right rounded-md bg-color7-light py-1 shadow-lg ring-1 ring-color6-lightgrey transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in min-w-20"
+                </div>
+              ) : (
+                <div className="flex gap-2 overflow-x-auto sm:w-full md:w-auto custom-scrollbar my-4">
+                  <Link
+                    to="/Register"
+                    className="mt-8 px-4 py-2 text-sm bg-color9-rose text-white rounded-md hover:bg-color8-grey"
                   >
-                    <MenuItem>
-                      <span
-                        className="block px-4 py-2 text-sm text-color5-dark data-[focus]:bg-color6-lightgrey data-[focus]:outline-none"
-                        onClick={() => logout()}
-                      >
-                        Logout
-                      </span>
-                    </MenuItem>
-                  </MenuItems>
-                </Menu>
-              </div>
-            ) : (
-              <div className="flex gap-2 overflow-x-auto sm:w-full md:w-auto custom-scrollbar my-4">
-                <Link
-                  to="/Register"
-                  className="mt-8 px-4 py-2 text-sm bg-color9-rose text-white rounded-md hover:bg-color8-grey"
-                >
-                  Register
-                </Link>
-                <Link
-                  to="/Login"
-                  className="mt-8 px-4 py-2 text-sm bg-color2-lightblue text-white rounded-md hover:bg-color8-grey"
-                >
-                  Login
-                </Link>
-              </div>
-            )}
-          </div>
+                    Register
+                  </Link>
+                  <Link
+                    to="/Login"
+                    className="mt-8 px-4 py-2 text-sm bg-color2-lightblue text-white rounded-md hover:bg-color8-grey"
+                  >
+                    Login
+                  </Link>
+                </div>
+              )}
+            </div>
+          )}
         </div>
       </div>
     </Disclosure>
